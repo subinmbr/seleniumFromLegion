2f2b9bce4bcdebd1106b604ba2cd9e7283029de1
subinmbr <subinmbr4@gmail.com>
2023-11-21 19:52:09 +0530

    2nd commit

---
+++ seleniumFromLegion/src/test/java/testScripts/XpathTests.java
@@ -0,0 +1,20 @@
+package testScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathTests {
+
+	public static void main(String[] args) {
+		 WebDriver driver  =  new ChromeDriver();
+		 
+		 driver.get("https://automationbookstore.dev/");
+		 
+		 driver.quit();
+		 
+		 
+		 
+
+	}
+
+}
